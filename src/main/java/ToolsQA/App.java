ce3289f1640123bfbf7a9aaa4807c07b2008ead0
Rishi Sathe <87525765+RishiSathe@users.noreply.github.com>
2021-07-24 14:13:08 +0530

    Update App.java

--- src/main/java/ToolsQA/App.java
+++ src/main/java/ToolsQA/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println("Maven Build")
+        System.out.println("Maven Build");
     }
 }
